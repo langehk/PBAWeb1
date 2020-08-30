038c639eecd79219ba9af707daa7116d42e3770f
langehk <32093132+langehk@users.noreply.github.com>
2020-08-30 20:24:23 +0200

    Pba web Assignment 15
    :fire:  :fire:  first commit :fire: :fire:

---
+++ js15.js
@@ -0,0 +1,14 @@
+// Henter input og konverterer..
+// Samt sætter value i andet input felt, mens det bliver cleared.
+function convertTemp() {
+  let c = document.getElementById("c"),
+    f = document.getElementById("f");
+
+  if (c.value != "") {
+    f.value = Math.round((c.value * 9) / 5 + 32);
+    c.value = "";
+  } else {
+    c.value = Math.round(((f.value - 32) * 5) / 9);
+    f.value = "";
+  }
+}
