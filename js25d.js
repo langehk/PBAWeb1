564d5b3ad7898e4e938b81fda2bd9336d1a8331e
langehk <32093132+langehk@users.noreply.github.com>
2020-08-30 21:36:44 +0200

    start 25d & c

---
+++ js25d.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Truth table for negation");
+console.log("var1\t!var1");
+var var1 = true;
+console.log(var1 + "\t" + !var1);
+var var1 = false;
+console.log(var1 + "\t" + !var1);
