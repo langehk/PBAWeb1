ddfe6f59bea825963d166b7e76166544e9d39c0e
langehk <32093132+langehk@users.noreply.github.com>
2020-08-31 07:35:57 +0200

    25c3 & d3

---
+++ js25c3.js
@@ -0,0 +1,7 @@
+console.log("____Opgave 25c3  ______");
+console.log("var11\tvar12\tvar13");
+let var11 = true;
+let var12 = true;
+let var13 = true;
+
+console.log(var11 + "\t" + var12 + "\t" + var13 + "\t");
