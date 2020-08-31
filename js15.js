8cce98600aa79ca51a3448852902562bef9ae3a8
langehk <32093132+langehk@users.noreply.github.com>
2020-08-31 10:21:59 +0200

    dav

--- js15.js
+++ js15.js
@@ -1,8 +1,14 @@
 // Henter input og konverterer..
 // Samt sætter value i andet input felt, mens det bliver cleared.
+
+console.log("HELLO LOG WORLD!");
+document.write("Hello document world!");
+
 function convertTemp() {
   let c = document.getElementById("c"),
     f = document.getElementById("f");
+
+    
 
   if (c.value != "") {
     f.value = Math.round((c.value * 9) / 5 + 32);
