ac4a0eaf881d8ea2cdc2ab79820ed4bfeac3c1a9
langehk <32093132+langehk@users.noreply.github.com>
2020-08-31 07:31:33 +0200

    Assignment 25c + d

--- js25c.js
+++ js25c.js
@@ -1,7 +1,22 @@
+/*
 "use strict";
-console.log("Truth table for negation");
+console.log("Truth Table Conjunction");
 console.log("var1\t!var1");
 var var1 = true;
+
 console.log(var1 + "\t" + !var1);
 var var1 = false;
 console.log(var1 + "\t" + !var1);
+*/
+
+console.log("____Opgave 25 C  ______");
+console.log("var1\tvar2\tvar1 && var2");
+let var1 = true;
+let var2 = true;
+console.log(var1 + "\t" + var2 + "\t" + (var1 && var2) + "\t");
+
+console.log(var1 + "\t" + !var2 + "\t" + (!var1 && var2) + "\t");
+
+console.log(!var1 + "\t" + var2 + "\t" + (!var1 && var2) + "\t");
+
+console.log(!var1 + "\t" + !var2 + "\t" + (!var1 && var2) + "\t");
--- js25d.js
+++ js25d.js
@@ -1,7 +1,19 @@
+/*
 "use strict";
-console.log("Truth table for negation");
+console.log("Truth Table for Disjunction");
 console.log("var1\t!var1");
 var var1 = true;
 console.log(var1 + "\t" + !var1);
 var var1 = false;
 console.log(var1 + "\t" + !var1);
+*/
+
+console.log("____Opgave 25 D  ______");
+console.log("var1\tvar2\tvar1 && var2");
+let var3 = true;
+let var4 = true;
+
+console.log(var3 + "\t" + var4 + "\t" + (var3 || !var4));
+console.log(var3 + "\t" + !var4 + "\t" + (var3 || !var4));
+console.log(!var3 + "\t" + var4 + "\t" + (var3 || !var4));
+console.log(!var3 + "\t" + !var4 + "\t" + (!var3 || !var4));
